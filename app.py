ddd41e1ac76c568a013f74f1c1d8f9cee49cacd3
ReturnOfTheLast <returnofthelast@gmail.com>
2023-11-14 14:00:04 +0100

    IFramed mongo-express and redis

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.get('/')
 def index():
     return render_template('index.html')
 
 
+@app.get('/mongo')
+def mongo():
+    return render_template('mongo.html')
+
+
+@app.get('/redis')
+def redis():
+    return render_template('redis.html')
+
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", 8080)
+    app.run('0.0.0.0', 8080)
