6c168f544bde44f87061b2cbf733f28346fcb33f
ReturnOfTheLast <returnofthelast@gmail.com>
2023-11-15 14:48:04 +0100

    Added notification api endpoint

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3
+# https://github.com/ReturnOfTheLast/p3_management_interface
+
+from flask import Flask, render_template, request, jsonify
+from gotify import Gotify
+import json
+
+with open('config.json', 'r') as fp:
+    config: dict = json.load(fp)
+
+gotifies: list[Gotify] = list()
+for token in config['gotify_tokens']:
+    gotifies.append(Gotify(
+        base_url=config['gotify_url'],
+        app_token=token
+    ))
 
 app = Flask(__name__)
 
 
+# --- [ Frontend ]
 @app.get('/')
 def index():
     return render_template('index.html')
@@ -16,6 +32,21 @@ def mongo():
 @app.get('/redis')
 def redis():
     return render_template('redis.html')
+
+
+# --- [ API ]
+@app.post('/api/notify')
+def notify():
+    req_json = request.get_json()
+    for gotify in gotifies:
+        gotify.create_message(
+            req_json["message"],
+            title=req_json["title"],
+            priority=req_json["priority"]
+        )
+    return jsonify({
+        "number_of_messages": len(gotifies)
+    })
 
 
 if __name__ == '__main__':
